4db35455fca0250926187d48476b58b42149ab5f
Davis Owen <davis.owen752@myci.csuci.edu>
2025-06-15 01:24:58 -0700

    Initial commit

---
+++ code/Wordle.java
@@ -0,0 +1,10 @@
+package wordleInJava.code;
+
+class Wordle{
+    public static void main(String[] args) {
+        System.out.println("WORDLE\n------\n");
+
+        String correct = "SHAKE";
+        String guess = "PASTE";
+    }
+}
